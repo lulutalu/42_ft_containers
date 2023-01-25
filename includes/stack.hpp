9a8a206e9420b5fa05e8937c54fbdf8b1aa87cc6
Loic Dubouloz <lulutalu@gmail.com>
2023-01-25 15:18:55 +0100

    Added all the prototypes for ft::stack

--- includes/stack.hpp
+++ includes/stack.hpp
@@ -6,11 +6,86 @@
 /*   By: lulutalu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 17:36:20 by lulutalu          #+#    #+#             */
-/*   Updated: 2022/12/10 17:36:32 by lulutalu         ###   ########.fr       */
+/*   Updated: 2023/01/25 15:18:30 by lduboulo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STACK_HPP
 # define STACK_HPP
+
+# include "vector.hpp"
+
+namespace ft {
+
+template <class T, class Container = ft::vector<T> >
+class stack
+{
+
+		public :
+
+				////////////////////////////////////////////////////////////////
+				///						Member Types						////
+				////////////////////////////////////////////////////////////////
+
+				typedef T				value_type;
+				typedef Container		container_type;
+				typedef std::size_t		size_type;
+
+				////////////////////////////////////////////////////////////////
+				///						Member Functions					////
+				////////////////////////////////////////////////////////////////
+
+/*				explicit stack(const container_type& cntr = container_type());  //Constructor
+
+				bool				empty(void) const;							// Boolean function to check if the container is empty
+				size_type			size(void) const;							// Return the size of the container
+				value_type&			top(void);									// Return a reference to the top value of the stack
+				const value_type&	top(void) const;
+				void				push(const value_type& val);				// Insert new element at the top of the stack
+				void				pop(void);									// Remove top element of the stack
+				void				swap(stack& x);								// Swap two stack
+*/
+
+		protected :
+
+				container_type		_cont;
+
+		public :
+
+}; // class
+
+		////////////////////////////////////////////////////////////////////////
+		///						Relational Operators						////
+		////////////////////////////////////////////////////////////////////////
+
+/*		template <class T, class Container>
+		bool	operator == (const stack<T, Container>& lhs, const stack<T, Container>& rhs);
+
+		template <class T, class Container>
+		bool	operator != (const stack<T, Container>& lhs, const stack<T, Container>& rhs);
+
+		template <class T, class Container>
+		bool	operator < (const stack<T, Container>& lhs, const stack<T, Container>& rhs);
+
+		template <class T, class Container>
+		bool	operator <= (const stack<T, Container>& lhs, const stack<T, Container>& rhs);
+
+		template <class T, class Container>
+		bool	operator > (const stack<T, Container>& lhs, const stack<T, Container>& rhs);
+
+		template <class T, class Container>
+		bool	operator >= (const stack<T, Container>& lhs, const stack<T, Container>& rhs);
+
+
+		////////////////////////////////////////////////////////////////////////
+		///							Functions Overload						////
+		////////////////////////////////////////////////////////////////////////
+
+		template <class T, class Container>
+		void	swap(stack<T, Container>& x, stack<T, Container>& y);
+*/
+
+
+} // namespace
 
 #endif
